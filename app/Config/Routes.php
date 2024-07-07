25353a496043fcff1a067174438d3e0c16f86f81
sitialifatulkh <sitialifatulkh@gmail.com>
2024-07-07 13:39:32 +0700

    editing controller dan routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,14 @@
 <?php
 
+
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/About', 'page::about');
+$routes->get('/Contact', 'page::contact');
+$routes->get('/Faqs', 'page::faqs');
+$routes->get('/Tos', 'page::tos');
+$routes->setAutoRoute(false);
---
+++ app/Controllers/Page.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        echo "about page";
+    }
+    public function contact()
+    {
+        echo "contact page";
+    }
+    public function faqs()
+    {
+        echo "faqs page";
+    }
+    public function tos()
+    {
+        echo "Halaman Term of Service";
+    }
+}
